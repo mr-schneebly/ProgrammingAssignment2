af652908f5f5bfce9128ccad9aabd86a93a6637b
mr-schneebly <aheels@precisionsportscience.com>
2024-10-24 15:18:10 -0700

    update for assisngment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions cache the inverse of a matrix to avoid costly
+## recomputation. The makeCacheMatrix function creates a special
+## "matrix" object that stores a matrix and its inverse (if calculated).
+## The cacheSolve function computes the inverse of the matrix only if
+## it hasn't already been cached.
 
-## Write a short comment describing this function
-
+## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL  # Initialize the inverse property
+  
+  # Method to set the matrix
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL  # Reset the inverse cache when the matrix is changed
+  }
+  
+  # Method to get the matrix
+  get <- function() x
+  
+  # Method to set the inverse of the matrix
+  setInverse <- function(inverse) inv <<- inverse
+  
+  # Method to get the inverse of the matrix
+  getInverse <- function() inv
+  
+  # Return a list of the methods
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## This function computes the inverse of the matrix returned by makeCacheMatrix.
+## If the inverse has already been calculated (and the matrix has not changed),
+## then cacheSolve retrieves the inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()  # Retrieve the cached inverse, if available
+  
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)  # Return the cached inverse
+  }
+  
+  # Otherwise, compute the inverse
+  mat <- x$get()  # Get the matrix
+  inv <- solve(mat, ...)  # Compute the inverse using solve()
+  
+  # Cache the inverse for future use
+  x$setInverse(inv)
+  
+  inv  # Return the inverse
 }
